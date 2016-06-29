018a829a75e922b88d849edf790b7ec7b2ea3bb3
Carolyn Daniel <carolyn.s.daniel@gmail.com>
2016-06-29 16:51:01 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Draw Together
+
+## Overview
+* A collaborative drawing app where multiple people on different devices can draw on the same canvas at the same time.
+
+### Technologies, frameworks, & programming Languages used
+* HTML5 & CSS3
+* Canvas
+* JavaScript
+* jQuery
+* AngularJS
+* Nodejs
+* Express
+* Socket.io
+
+
+### Url to live demo
---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.use(express.static('public'));
+
+io.on('connection', function(clientSocket) {
+
+  clientSocket.on('draw', function(coords) {
+    io.emit('draw', coords);
+    // console.log(coords);
+  });
+
+});
+
+server.listen(3000, function() {
+  console.log('Listening on 3000...');
+});
